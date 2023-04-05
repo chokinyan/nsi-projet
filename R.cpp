f5f742897523fae78db956249c8129acff7963c4
chokinyan <sabou2344@gmail.com>
2023-04-05 12:47:25 +0200

    quelque test c++

--- R.cpp
+++ R.cpp
@@ -2,8 +2,29 @@
 #include <math.h>
 using namespace std;
 
+class test{
+    public:
+        string te;
+        int waw;
+        test(string x,int y);
+        int lol();
+};
+
+test::test(string x,int y){
+    waw = y;
+    te = x;
+};
+
+int test::lol(){
+    return waw ++;
+};
+
+
 int main(){
-    float x = sqrt(3);
-    cout << x ;
+    test x("ok",4);
+    for (int i; i<6;i++){
+        x.lol();
+    };
+    cout << x.waw;
     return 0;
 }
