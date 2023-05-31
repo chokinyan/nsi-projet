97689a6a8cfa09f655f7662cc872d18ddd383d69
chokinyan <sabou2344@gmail.com>
2023-05-31 20:44:35 +0200

    update secret

--- trucsecret.py
+++ trucsecret.py
@@ -1,12 +1,33 @@
 import pygame
 import random
+import keyboard
 
+
+# 90 case donc un jeu de 10*10
+# A-J ↓↑  || 1-10 ←→
+# 1 Porte-avions (5 cases) ;
+# 1 Croiseur (4 cases) ;
+# 2 Contre-torpilleurs (3 cases) ;
+# 1 Torpilleur (2 cases).
 
 def main():
     pygame.init()
-    ecran = pygame.display.set_mode((1920,1080))
+    ecran = pygame.display.set_mode((1280,720))
+    pygame.display.set_caption("Bataille navale")
     end = False
+    plateau_joueur = [[-1 for i in range(10)] for i in range(10)]
+    plateau_bot = [[-1 for i in range(10)] for i in range(10)]
+    plateau_rect = [[] for i in range(10)]
+    for j in range(10):
+        for i in range(10):
+            rect = pygame.Rect(i*ecran.get_width()/10+1,j*ecran.get_height()/10+1,ecran.get_width()/10-5,ecran.get_height()/10-5)
+            plateau_rect[j].append(rect)
+            pygame.draw.rect(ecran,(255,255,255),rect)
+    pygame.display.flip()
     while not(end):
+        if keyboard.is_pressed("Esc"):
+            pygame.quit()
+            quit()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 end = True
