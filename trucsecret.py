b8e900bebecbb69b413713e9ae202a48b9a853a3
chokinyan <sabou2344@gmail.com>
2023-06-01 10:49:27 +0200

    update secret

--- trucsecret.py
+++ trucsecret.py
@@ -10,36 +10,6 @@ import keyboard
 # 2 Contre-torpilleurs (3 cases) ;
 # 1 Torpilleur (2 cases).
 
-def main():
-    pygame.init()
-    ecran = pygame.display.set_mode((1720,780))
-    pygame.display.set_caption("Bataille navale")
-    end = False
-    plateau_rect = phase_jeu(1,ecran)
-    plateau_joueur = [[0 for i in range(10)] for i in range(10)]
-    plateau_bot = [[0 for i in range(10)] for i in range(10)]
-    tour = 0
-    piece_placer = 0
-    croix_pas_toucher = None
-    croix_toucher = None
-    pygame.draw.
-    pygame.display.flip()
-    while not(end):
-        for collone in plateau_rect:
-            for case in collone:
-                if case.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
-                    print("ok")
-                    pygame.time.wait(110)
-        if keyboard.is_pressed("Esc"):
-            pygame.quit()
-            quit()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                end = True
-
-    pygame.quit()
-    quit()
-
 def phase_jeu(phase,ecran):
     if phase == 1:
         plateau_rect = [[] for i in range(10)]
@@ -52,10 +22,62 @@ def phase_jeu(phase,ecran):
     elif phase == 2:
         pass
 
+pygame.init()
+ecran = pygame.display.set_mode((1720,780))
+pygame.display.set_caption("Bataille navale")
+end = False
+plateau_rect = [[] for i in range(10)]
+for j in range(10):
+    for i in range(10):
+        rect = pygame.Rect(i*80,j*80,75,75)
+        plateau_rect[j].append(rect)
+        pygame.draw.rect(ecran,(255,255,255),rect)
+plateau_joueur = [[0 for i in range(10)] for i in range(10)]
+plateau_bot = [[0 for i in range(10)] for i in range(10)]
+tour = 0
+piece_placer = 0
+#max 5
+Porte_avions = None
+Croiseur = None
+Contre_torpilleurs1 = None
+Contre_torpilleurs2 = None
+Torpilleur = None
+toucher = False
+joueur_joue = False
+game_play = False
+tour_texte_fond = pygame.font.SysFont("None",100)
+tour_texte = tour_texte_fond.render(f"tour numero : {str(tour)}",False,(255,255,255))
+tour_rect = pygame.rect.Rect(925,0,575,68)
+tour_surf = ecran.subsurface(tour_rect)
+tour_surf.blit(tour_texte,(0,0))
+#ecran.blit(tour_texte,(ecran.get_width()-(plateau_rect[0][-1].x + plateau_rect[0][-1].width),0))
+ecran.blit(tour_surf.copy(),tour_rect)
+pygame.display.flip()
+while not(end):
+    if game_play:
+        for collone in plateau_rect:
+            for case in collone:
+                if case.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+                    if toucher:
+                        pygame.draw.line(ecran,(255,0,0),(case.x,case.y),(case.x + case.width,case.y + case.height),width=3)
+                        pygame.draw.line(ecran,(255,0,0),(case.x,case.y + case.height),(case.x + case.width,case.y),width=3)
+                    else:
+                        pygame.draw.line(ecran,(0,0,0),(case.x,case.y),(case.x + case.width,case.y + case.height),width=3)
+                        pygame.draw.line(ecran,(0,0,0),(case.x,case.y + case.height),(case.x + case.width,case.y),width=3)
+                    tour += 1
+                    tour_texte = tour_texte_fond.render(f"tour numero : {str(tour)}",False,(255,255,255))
+                    tour_surf.fill((0,0,0))
+                    tour_surf.blit(tour_texte,(0,0))
+                    ecran.blit(tour_surf.copy(),tour_rect)
+                    pygame.display.flip()
+                    pygame.time.wait(250)
 
+    if keyboard.is_pressed("Esc"):
+        pygame.quit()
+        quit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            end = True
 
-
-
-
-if __name__ == "__main__":
-    main()
+pygame.quit()
+quit()
