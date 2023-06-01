43008a73c6e8ef5f20291499c70e7814df300638
chokinyan <sabou2344@gmail.com>
2023-06-01 11:44:42 +0200

    update secret

--- trucsecret.py
+++ trucsecret.py
@@ -29,7 +29,7 @@ end = False
 plateau_rect = [[] for i in range(10)]
 for j in range(10):
     for i in range(10):
-        rect = pygame.Rect(i*80,j*80,75,75)
+        rect = pygame.Rect(i*77,j*77,73,73)
         plateau_rect[j].append(rect)
         pygame.draw.rect(ecran,(255,255,255),rect)
 plateau_joueur = [[0 for i in range(10)] for i in range(10)]
@@ -50,10 +50,15 @@ tour_texte = tour_texte_fond.render(f"tour numero : {str(tour)}",False,(255,255,
 tour_rect = pygame.rect.Rect(925,0,575,68)
 tour_surf = ecran.subsurface(tour_rect)
 tour_surf.blit(tour_texte,(0,0))
-#ecran.blit(tour_texte,(ecran.get_width()-(plateau_rect[0][-1].x + plateau_rect[0][-1].width),0))
 ecran.blit(tour_surf.copy(),tour_rect)
-pygame.display.flip()
+#75*75 les case
+txt_surf = ecran.subsurface()
+texte_fond = pygame.font.SysFont("None",70)
+text = texte_fond.render("Click sur le bouton pour commencer",False,(255,0,0))
+text_rect = pygame.rect.Rect((plateau_rect[3][-1].x + plateau_rect[3][-1].width + 20,plateau_rect[3][-1].y),(500,plateau_rect[6][-1].y))
+ecran.blit(text,text_rect)
 while not(end):
+    pygame.display.flip()
     if game_play:
         for collone in plateau_rect:
             for case in collone:
@@ -78,6 +83,5 @@ while not(end):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             end = True
-
 pygame.quit()
 quit()
